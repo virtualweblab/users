deb61802705f7af06b954f8705345796bd6ea9ef
mauricio duque orozco <mauricio.duque.eje@icloud.com>
2016-06-02 21:30:42 -0500

    comunicate

--- serial-socket/index.js
+++ serial-socket/index.js
@@ -6,24 +6,22 @@ var tcp = require('./tcp')
 module.exports = function (http){
 	var io = require('socket.io')(http)
 	var net = require('net');
-	var data_tcp = new tcp()
+	var data_tcp = new tcp(1338)
+
 	io.on('connection', function(socket){
       console.log('User Connect ' +  socket.id)
       socket.on('disconnect', function(){
 	    console.log('user disconnected');
-	  });
-		// data_tcp.on('data', function(data){
-		// 	console.log(data)
-		// })
+	});
 
-		socket.on('serialCom', function(msg){
-	        console.log(msg)
-	        //io.emit('serialCom', msg)
-					data_tcp.on('data', function(data){
-						console.log(data + 'l')
-						var dataTCP = data + '/'
-						io.emit('serialCom', dataTCP)
-					})
-	  })
+			socket.on('serialCom', function(msg){
+		        console.log(msg)
+		        //io.emit('serialCom', msg)
+						data_tcp.on('data', function(data){
+							console.log(data + 'l')
+							var dataTCP = data + '/'
+							io.emit('serialCom', dataTCP)
+						})
+		  })
   })
 }
--- serial-socket/tcp.js
+++ serial-socket/tcp.js
@@ -1,10 +1,14 @@
 
 var net = require('net');
-var events = require('events')
+var util = require('util')
+//var events = require('events')
+var EventEmitter = require('events').EventEmitter;
 
-module.exports = function(){
-	var data_tcp = new events.EventEmitter()
-	var server = net.createServer(function(socket) {
+var socketTCP= function(port){
+	this.port = port
+	//var data_tcp = new events.EventEmitter()
+	var data_tcp = this
+	this.server = net.createServer(function(socket) {
 		socket.name = socket.remoteAddress + ':' + socket.remotePort
 		socket.on('data', function(data){
 			//console.log('data: ' + data)
@@ -14,46 +18,9 @@ module.exports = function(){
 		socket.write('Echo server Mauri\r\n');
 	});
 
-	server.listen(1338, '127.0.0.1');
+	this.server.listen(this.port, '127.0.0.1');
 	return data_tcp
 }
+util.inherits(socketTCP, EventEmitter)
 
-//console.log('socket')
-// var tcp = require('./tcp')
-// var data_tcp = tcp()
-
-
-// io.on('connection', function(socket){
-//     console.log('User Connect ' +  socket.id)
-//     socket.on('disconnect', function(){
-//     console.log('user disconnected');
-//   });
-// 	// data_tcp.on('data', function(data){
-// 	// 	console.log(data)
-// 	// })
-//
-// 	data_tcp.getValueSocket(function(data){
-// 		console.log(data)
-// 	})
-//
-// 	socket.on('serialCom', function(msg){
-//         console.log(msg)
-//         io.emit('serialCom', msg)
-//   })
-// })
-
-// var io = require('socket.io')(http)
-// io.on('connection',function(webSocket){
-// 	console.log('User Conected: ' + webSocket.id)
-// 	webSocket.on('disconnect', function(){
-// 		console.log('User disconnected: ' + webSocket.id)
-// 	})
-// })
-	// var server = net.createServer(function(socketTCP){
-	// 	socketTCP.name = socketTCP.remoteAddress + ':' + socketTCP.remotePort
-	// 	socketTCP.on('data', function(dataTCP){
-	// 		console.log(dataTCP)
-	// 		socketTCP.write('Data Ok')
-	// 	})
-	// })
-	// server.listen(1338, '127.0.0.1');
+module.exports = socketTCP
